3973d257abd82745f6c82e4888cb87a8adf7ab14
Daniel <onyemordidaniel@gmail.com>
2025-08-10 11:33:04 +0100

    all

--- src/app/reuseables/http-loader/request-data.service.ts
+++ src/app/reuseables/http-loader/request-data.service.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../auth/auth.service'
 export class RequestDataService {
   // private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
   private baseUrl = 'https://fbapp01-125e9985037c.herokuapp.com/api'; // Replace with your API endpoint
-// 
+// dfg
   constructor(
     private http: HttpClient,
     private loader: LoaderService,
