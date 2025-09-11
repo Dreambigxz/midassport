022fc6db10579d0bca82aa0c0b64f348cff14aab
Daniel <onyemordidaniel@gmail.com>
2025-09-11 08:21:35 +0100

    all

--- src/app/reuseables/http-loader/request-data.service.ts
+++ src/app/reuseables/http-loader/request-data.service.ts
@@ -6,8 +6,8 @@ import { AuthService } from '../auth/auth.service'
 
 @Injectable({ providedIn: 'root' })
 export class RequestDataService {
-  // private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
-  private baseUrl = 'https://goldenball-7df3848b33fb.herokuapp.com/api'; // Replace with your API endpoint
+  private baseUrl = 'http://127.0.0.1:8000/api'; // Replace with your API endpoint
+  // private baseUrl = 'https://goldenball-7df3848b33fb.herokuapp.com/api'; // Replace with your API endpoint
 // dfg
   constructor(
     private http: HttpClient,
